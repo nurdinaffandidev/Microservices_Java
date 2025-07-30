ef2636b723b28ae7994f0f3828e0e1a000b35bc7
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-07-30 14:14:46 +0800

    feat[question]: create global exception handler, api error class

---
+++ src/main/java/com/nurdinaffandidev/QuizApp/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.nurdinaffandidev.QuizApp.exceptionHandler;
+
+import com.nurdinaffandidev.QuizApp.question.exception.CategoryNotFoundException;
+import com.nurdinaffandidev.QuizApp.question.exception.QuestionNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<ApiError> handleCategoryNotFound(CategoryNotFoundException exception) {
+        ApiError error = new ApiError(
+                            exception.getMessage(),
+                            HttpStatus.NOT_FOUND.value(),
+                            LocalDateTime.now());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND );
+    }
+
+    @ExceptionHandler(QuestionNotFoundException.class)
+    public ResponseEntity<ApiError> handleQuestionNotFound(QuestionNotFoundException exception) {
+        ApiError error = new ApiError(
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception exception) {
+        return new ResponseEntity<>("Internal Server Error: " + exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
